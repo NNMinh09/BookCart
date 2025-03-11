24d4e3c60d3f0e45bca030ccc1fc478c6e7b4358
NNMinh09 <nguyennhutminh742004@gmail.com>
2025-03-11 07:46:11 +0700

    Initial commit

---
+++ BookCart/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using BookCart.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCart.Controllers
+{
+    public class UserController : Controller
+    {
+        readonly BookCartDbContext _ctx;
+        public UserController(BookCartDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = _ctx.Users.ToListAsync();
+            return View(users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create(UserController user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _ctx.Users.AddAsync(user);
+                await _ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+    }
+}
---
+++ BookCart/Data/BookCartdbContextcs.cs
@@ -0,0 +1,16 @@
+﻿using BookCart.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookCart.Data
+{
+    public class BookCartDbContext : DbContext
+    {
+        public BookCartDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
